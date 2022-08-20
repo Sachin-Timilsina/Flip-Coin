04224b9c4443eb4cf0ced62ce3cb216fef12b3c5
Sachin Timilsina <sachintimilsina11@gmail.com>
2022-08-20 16:27:42 +0545

    let's commit

---
+++ app/src/main/java/com/example/flipcoin/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.flipcoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flipButton: Button = findViewById(R.id.button)
+
+        flipButton.setOnClickListener {
+            flipCoin()
+        }
+}
+
+    private fun flipCoin() {
+        val coin = Coin()
+        val coinFlip = coin.flipCoin()
+        val imageCoin: ImageView =findViewById(R.id.imageView)
+
+        if (coinFlip == 1) {
+            imageCoin.setImageResource(R.drawable.heads)
+            imageCoin.contentDescription = coinFlip.toString()
+        }
+        else{
+            imageCoin.setImageResource(R.drawable.tails)
+            imageCoin.contentDescription = coinFlip.toString()
+        }
+
+    }
+
+class Coin {
+    fun flipCoin():Int{
+        return (1..2).random()
+    }
+
+}}
